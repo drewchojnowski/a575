3be5c489b3d12fcf2a0a6beb22650d6e8aa14a27
Drew Chojnowski <drewski@alphaboo.nmsu.edu>
2015-09-29 18:01:33 -0600

    q18 updates

--- q18/fun_with_loops.f95
+++ q18/fun_with_loops.f95
@@ -1,32 +1,33 @@
-! This program does not work !
-
-module fact_mod
-contains
-    function factorial(x)
-        integer :: i,x
-        real (kind(1.0d0)) :: factorial
-        factorial=1
-        do i=2,x
-            factorial=factorial*i
-        enddo
-    end function
-end module
+!===================================================================
+! OUTPUT:
+!  [drewski@alphaboo q18]$ fun_with_loops
+!     when x was                     1 , factorial was                     1
+!     when x was                     3 , factorial was                     6
+!     when x was                    13 , factorial was            6227020800
+!     final x value:           12454041601
+!     # of times the loop executed:                     3
+!     sum of the factorials:            6227020807
+!===================================================================
 
 program fun_with_loops
-    use fact_mod
-    ! this is a comment
+    ! factorial-associated variables
+    integer(8) :: i=2,nfac=1
+    ! counters
+    integer(8) :: y=0,iter_count=0
+    ! variables
+    integer(8) :: x=1,factorial_sum=0
 
-    integer :: x,y,iter_count
-    x=1
-    y=0
-    iter_count=0
-
-    do while (x <= 100)
-        if (x < 20) then
+    do while (x.le.100)
+        if (x.lt.20) then
             y=x
-            x=factorial(x)
+            nfac=1
+            do i=2, x
+                nfac=nfac*i
+            enddo
+            x=nfac
             print *,'when x was ',y,', factorial was ',x
-        end if
+            factorial_sum=factorial_sum+x
+        endif
         x=(x*2)+1
         iter_count=iter_count+1
     enddo
@@ -34,5 +35,4 @@ program fun_with_loops
     print *,'final x value: ',x
     print *,'# of times the loop executed: ',iter_count
     print *,'sum of the factorials: ',factorial_sum
-
 end

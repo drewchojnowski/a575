445cebf7807b2a049105b3dd660dad4a985b60da
Drew Chojnowski <drewski@alphaboo.nmsu.edu>
2015-10-06 10:49:25 -0600

    adding partial q22

--- isochrone/isochrones.py
+++ isochrone/isochrones.py
@@ -1,14 +1,32 @@
 import numpy as np
 from astropy.io import ascii
 
-def get_isochrone_struct(file,quantities):
-    data=ascii.read(file)
-    str={'z':data['col1'],'log_age':data['col2'],'m_ini':data['col3'],'m_act':data['col4'],
-         'log_l':data['col5'],'log_t':data['col6'],'log_g':data['col7'],'mbol':data['col8'],
-         'u':data['col9'],'b':data['col10'],'v':data['col11'],'r':data['col12'],'i':data['col13'],
-         'j':data['col14'],'h':data['col15'],'k':data['col16'],'int_imf_stage':data['col17']}
+def get_isochrone_struct():
+    data=ascii.read('zp00.dat')
+    quantities=['logL/Lo','logTe']
+    cols=data.colnames
+    nquant=len(quantities)
 
-    print quantities
+    print cols
 
-    return str
+    ind=[0]*nquant
+    count=0
+    lst=''
+    for x in quantities:
+        t=cols.index(x)
+        ind[count]=t  
+
+        count=count+1
+
+    print data[ind]
+
+#    str={'z':data['col1'],'log_age':data['col2'],'m_ini':data['col3'],'m_act':data['col4'],
+#         'log_l':data['col5'],'log_t':data['col6'],'log_g':data['col7'],'mbol':data['col8'],
+#         'u':data['col9'],'b':data['col10'],'v':data['col11'],'r':data['col12'],'i':data['col13'],
+#         'j':data['col14'],'h':data['col15'],'k':data['col16'],'int_imf_stage':data['col17']}
+
+#    cols=str.keys()
+#    print quantities
+
+    return data
 

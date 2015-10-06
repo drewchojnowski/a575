8d9b3b7699187c45a0c6190badfe510c183e48c9
Drew Chojnowski <drewski@alphaboo.nmsu.edu>
2015-10-06 16:42:31 -0600

    updating q22/isochrone

--- isochrone/isochrones.py
+++ isochrone/isochrones.py
@@ -1,17 +1,10 @@
 import numpy as np
 from astropy.io import ascii
 
-# infile='zp00.dat'
-# quantities=['logL/Lo','logTe']
-
-def get_isochrone_struct(infile,quantities):
+def get_isochrone_struct(infile,quantities=['Z','log(age/yr)','logTe','logL/Lo','int_IMF','stage'],age=8):
     data=ascii.read(infile)
+    gd=np.where(data['log(age/yr)']==age)
+    data=data[gd]
     data.keep_columns(quantities)
 
-#   str={'z':data['col1'],'log_age':data['col2'],'m_ini':data['col3'],'m_act':data['col4'],
-#         'log_l':data['col5'],'log_t':data['col6'],'log_g':data['col7'],'mbol':data['col8'],
-#         'u':data['col9'],'b':data['col10'],'v':data['col11'],'r':data['col12'],'i':data['col13'],
-#         'j':data['col14'],'h':data['col15'],'k':data['col16'],'int_imf_stage':data['col17']}
-
     return data
-

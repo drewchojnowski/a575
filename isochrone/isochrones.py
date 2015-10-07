59b2b3fa373822f995bc5101dec78d6fdd5c788c
Drew Chojnowski <drewski@alphaboo.nmsu.edu>
2015-10-07 14:34:56 -0600

    updating isochrone

--- isochrone/isochrones.py
+++ isochrone/isochrones.py
@@ -1,19 +1,21 @@
 import numpy as np
 from astropy.io import ascii
 
-def get_isochrone_struct(infile,quantities=['None'],age=['None']):
+def get_isochrone_struct(infile,quantities=None,age=None):
     data=ascii.read(infile)
 
     # option to select a certain age
-    if (age!='None'):
+    if age is not None:
         gd=np.where(data['log(age/yr)']==age)
         data=data[gd]
 
     # default columns
-    if (quantities=='None'):
-        quantities=['Z','log(age/yr)','logTe','logL/Lo','int_IMF','stage']
+    if quantities is None:
+        data.keep_columns(['Z','log(age/yr)','logTe','logL/Lo','int_IMF','stage'])
+    else:
+        data.keep_columns(quantities)
 
     # option to request certain columns
-    data.keep_columns(quantities)
+#    data.keep_columns(quantities)
 
     return data

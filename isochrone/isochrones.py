e059b8c2f3eb7fef620d460d5e467de45f152d2a
Drew Chojnowski <drewski@alphaboo.nmsu.edu>
2015-10-07 14:53:06 -0600

    adding q22

--- isochrone/isochrones.py
+++ isochrone/isochrones.py
@@ -1,7 +1,7 @@
 import numpy as np
 from astropy.io import ascii
 
-def get_isochrone_struct(infile,quantities=None,age=None):
+def get_isochrone_struct(infile,quantities=None,age=None,outfile=None):
     data=ascii.read(infile)
 
     # option to select a certain age
@@ -12,10 +12,16 @@ def get_isochrone_struct(infile,quantities=None,age=None):
     # default columns
     if quantities is None:
         data.keep_columns(['Z','log(age/yr)','logTe','logL/Lo','int_IMF','stage'])
+    # option to extract specified columns
     else:
         data.keep_columns(quantities)
 
-    # option to request certain columns
-#    data.keep_columns(quantities)
+    # option to write an output file
+    if outfile is not None:
+        cols=data.colnames
+        header=''
+        for col in cols:
+            header=header+'   '+col
+        np.savetxt(outfile,data,fmt='%.5f',header=header)
 
     return data

b62ceed6ec351312685037d55abb5655093f5034
Drew Chojnowski <drewski@alphaboo.nmsu.edu>
2015-11-03 18:34:17 -0700

    updating isochrone module

--- isochrone/isochrones.py
+++ isochrone/isochrones.py
@@ -25,3 +25,47 @@ def get_isochrone_struct(infile,quantities=None,age=None,outfile=None):
         np.savetxt(outfile,data,fmt='%.5f',header=header)
 
     return data
+
+def plot_iso(infile,age=None,outfile=None,show=False):
+    import matplotlib.pyplot as plt
+
+    data=ascii.read(infile)
+
+    # option to select a certain age
+    if age is not None:
+        gd=np.where(data['log(age/yr)']==age)
+        data=data[gd]
+
+    fig, axes = plt.subplots(2,2)
+    ax0, ax1, ax2, ax3 = axes.flat
+
+    plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.4, hspace=0.1)
+
+    ax0.plot(data['logTe'],data['logG'])
+    ax2.plot(data['logTe'],data['mbol'])
+    ax1.plot(data['logTe'],data['logG'])
+    ax3.plot(data['logTe'],data['logG'])
+
+    ax2.set_xlabel(r'Log $\rm T_{eff}$')
+    ax3.set_xlabel(r'Log $\rm T_{eff}$')
+
+    ax0.set_ylabel('Log g')
+    ax2.set_ylabel('Mbol')
+    ax1.set_ylabel('Log g')
+    ax3.set_ylabel('Log g')
+
+    ax0.set_xlim((4.2,3.4))
+    ax2.set_xlim((4.2,3.4))
+    ax1.set_xlim((4.2,3.4))
+    ax3.set_xlim((4.2,3.4))
+
+    ax0.set_xticklabels([])
+    ax1.set_xticklabels([])
+
+    # option to write an output file
+    if outfile is not None:
+        plt.savefig(outfile)
+
+    if show: plt.show()
+
+    return data
